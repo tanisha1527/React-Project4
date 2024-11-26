3e977b18a45fe56366d68fdc1ad4bffcddfd1d43
tanisha1527 <vermatanisha620@gmail.com>
2024-11-27 01:10:36 +0530

    Added datamapping for Today's biggest hits in displayHome component

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './Navbar'
 import { albumsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
+import { songsData } from '../assets/assets'
+import SongItem from './SongItem'
 
 const DisplayHome = () => {
   return (
@@ -11,6 +13,12 @@ const DisplayHome = () => {
         <h4 className='my-5 font-bold text-2xl'>Featured Charts</h4>
           <div className='flex overflow-auto'>
           {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+          </div>
+      </div>
+      <div className='mb-4'>
+        <h4 className='my-5 font-bold text-2xl'>Today's biggest hits</h4>
+          <div className='flex overflow-auto'>
+             {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
           </div>
       </div>
     </>
